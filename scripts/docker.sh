283df95db0158b215e4fcc29e08d98c53c8c6a67
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2020-10-05 17:05:56 +0200

    workflows: replace pushd with cd & cd -

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,8 +10,9 @@ if [ -z "$tag" ]; then
 fi
 
 echo $image:$tag
-pushd docker/
+cd docker/
 docker buildx create --name mybuilder --use || echo 'skip'
 docker buildx inspect --bootstrap
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t $image:$tag --push .
 docker buildx imagetools inspect $image:$tag
+cd -
