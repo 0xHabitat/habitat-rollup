6312d18189f5a0387711fd046adc1bfd5f6c7d29
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-03-09 13:07:53 +0100

    web: fix error message

--- web/lib/utils.js
+++ web/lib/utils.js
@@ -158,7 +158,7 @@ export function wrapListener (selectorOrElement, func, eventName = 'click') {
     try {
       await func(evt);
     } catch (e) {
-      alertModal((e.error ? e.error.message : '') || e.toString());
+      alertModal((e.error ? e.error.message : '') || e.message || e.toString());
     }
 
     evt.target.disabled = false;
