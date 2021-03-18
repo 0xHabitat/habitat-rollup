c4b83fae958800aa991cec9de1b3c3370d976986
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-03-18 20:35:05 +0100

    web/lp: updates

--- web/landing/lp/index.js
+++ web/landing/lp/index.js
@@ -1,27 +1,154 @@
 import {
   wrapListener,
   renderAmount,
+  walletIsConnected,
+  getSigner,
+  getProvider,
+  renderAddress,
+  getEtherscanLink,
 } from '/lib/utils.js';
+import {
+  HBT,
+} from '/lib/config.js';
+
+import { ethers } from '/lib/extern/ethers.esm.min.js';
+
+const UNISWAP_PAIR = '0xc7a1cb6edc22e94f17c80eb5b959f2ad28511d4e';
+const MINT_TOPIC = '0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f';
+const BURN_TOPIC = '0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496';
+const TRANSFER_TOPIC = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
+const START_BLOCK = 12_010_274;
 
 const ONE_WEEK = 3600 * 24 * 7; // seconds
 const MAX_WEEKS = ONE_WEEK * 12;
 const MAX_HBT = 2_000_000;
 const MAX_SHARE = Math.sqrt(MAX_HBT) * (MAX_WEEKS * MAX_WEEKS);
-const MAX_REWARD = 1200;
+const MAX_REWARD_DAY = 2143;
 const DAY = 3600 * 24;
+const START_DATE = 1615372251;
+const END_DATE = 1615372251 + MAX_WEEKS;
 
-function calcDailyReward (amount) {
-  let cumulative = 0;
+const globalBlocks = JSON.parse(localStorage.getItem('gblocks') || '{}');
+const globalReceipts = JSON.parse(localStorage.getItem('greceipts') || '{}');
+
+function calcDailyReward (amount, duration) {
   const rewards = [];
+  let cumulative = 0;
 
-  for (let seconds = 0; seconds < MAX_WEEKS; seconds += DAY) {
+  for (let seconds = 0; seconds < duration; seconds += DAY) {
     const share = (Math.sqrt(amount) * (seconds * seconds)) / MAX_SHARE;
-    const r = MAX_REWARD * share;
-    rewards.push(r);
+    const r = MAX_REWARD_DAY * share;
     cumulative += r;
+    rewards.push(r);
   }
 
   return { cumulative, rewards };
+}
+
+async function update () {
+  let account;
+  if (walletIsConnected()) {
+    const signer = await getSigner();
+    account = await signer.getAddress();
+  }
+
+  const provider = getProvider();
+  const eventFilter = {
+    address: UNISWAP_PAIR,
+    topics: [[MINT_TOPIC, BURN_TOPIC]],
+    fromBlock: `0x${START_BLOCK.toString(16)}`,
+    toBlock: 'latest',
+  };
+
+  // fetch the mint & burn events and gather timestamps
+  const logs = await provider.send('eth_getLogs', [eventFilter]);
+  let tmp = [];
+  for (const log of logs) {
+    const block = globalBlocks[log.blockHash] || await provider.getBlock(log.blockHash);
+    globalBlocks[log.blockHash] = block;
+    const timestamp = Number(block.timestamp);
+
+    if (timestamp > END_DATE) {
+      // done
+      break;
+    }
+
+    const receipt = globalReceipts[log.transactionHash] || await provider.getTransactionReceipt(log.transactionHash);
+    globalReceipts[log.transactionHash] = receipt;
+    tmp.push({ log, receipt, timestamp });
+  }
+
+  // save/cache
+  localStorage.setItem('gblocks', JSON.stringify(globalBlocks));
+  localStorage.setItem('greceipts', JSON.stringify(globalReceipts));
+
+  // filter the events and extract the token amounts
+  const accountMap = {};
+  for (const ele of tmp) {
+    const from = ele.receipt.from;
+    const timestamp = ele.timestamp;
+    let hbtValue;
+    for (const log of ele.receipt.logs) {
+      // remember the last token transfer
+      if (log.topics[0] === TRANSFER_TOPIC && log.address.toLowerCase() === HBT.toLowerCase()) {
+        hbtValue = Number(ethers.utils.formatUnits(log.data, '10'));
+        continue;
+      }
+      if (log.address.toLowerCase() !== UNISWAP_PAIR) {
+        // skip if target is not the uniswap pair
+        continue;
+      }
+      const isMint = log.topics[0] === MINT_TOPIC;
+      const isBurn = log.topics[0] === BURN_TOPIC;
+      if (isMint || isBurn) {
+        const ary = accountMap[from] || [];
+        ary.push({ isMint,isBurn, hbtValue, timestamp });
+        accountMap[from] = ary;
+      }
+    }
+  }
+
+  // calculate the average for each user
+  const now = ~~(Date.now() / 1000);
+  let res = [];
+  for (const addr in accountMap) {
+    const events = accountMap[addr];
+    let liquidity = 0;
+    let lastTime = 0;
+    let reward = 0;
+    for (const evt of events) {
+      if (!lastTime) {
+        // init
+        lastTime = evt.timestamp;
+      }
+      // calculate the reward since last time
+      const snapshot = calcDailyReward(liquidity, evt.timestamp - lastTime).cumulative;
+      lastTime = evt.timestamp;
+      reward += snapshot;
+
+      if (evt.isMint) {
+        liquidity += evt.hbtValue;
+      } else {
+        liquidity -= evt.hbtValue;
+      }
+    }
+    // calculate remaining reward up to now
+    reward += calcDailyReward(liquidity, now - lastTime).cumulative;
+    // and the max reward if the liquidity stays in until `END_DATE`
+    const maxReward = reward + calcDailyReward(liquidity, END_DATE - now).cumulative;
+    res.push({ addr, reward, maxReward, liquidity });
+  }
+
+  // now sort this stuff and render it
+  res = res.sort((a, b) => b.maxReward - a.maxReward);
+  let html = '<p>Rank</p><p>Address</p><p>Current Reward</p><p>Expected Reward</p><p>Liquidity (Average)</p>';
+  for (let i = 0, len = res.length; i < len; i++) {
+    const { addr, reward, maxReward, liquidity } = res[i];
+    const highlight = account && addr === account;
+    html +=
+      `<p>${highlight ? '<bold>🌟' + (i + 1) + '</bold>' : i + 1}.</p><p>${renderAddress(addr)}</p><p>${renderAmount(reward)} HBT</p><p>${renderAmount(maxReward)} HBT</p><p>${renderAmount(liquidity)} HBT</p>`;
+  }
+  document.querySelector('#leaderboard').innerHTML = html;
 }
 
 async function render () {
@@ -40,19 +167,21 @@ async function render () {
   input.max = MAX_HBT;
 
   function draw () {
+    const now = ~~(Date.now() / 1000);
+
+    if (now > END_DATE) {
+      document.querySelector('#minting').style.display = 'none';
+      document.querySelector('#timeover').style.display = 'block';
+      return;
+    }
+
     const amount = Number(input.value);
-    const { cumulative, rewards } = calcDailyReward(amount);
+    const duration = END_DATE - now;
+    const { rewards, cumulative }= calcDailyReward(amount, duration);
     const len = rewards.length;
-    const maxReward = renderAmount(MAX_REWARD);
 
     description.textContent =
-      `Daily max. Reward is ${maxReward} HBT for up to ${MAX_WEEKS / ONE_WEEK} Weeks.\nThe sum of all your rewards are ${renderAmount(cumulative)} HBT.`;
-    let html = '';
-    for (let day = 0; day < len; day++) {
-      const v = renderAmount(rewards[day]);
-      html += `<p>Day ${day + 1}</p><p>${v} HBT</p>`;
-    }
-    grid.innerHTML = html;
+      `The sum of the maximum reward are ${renderAmount(cumulative)} HBT by providing ${renderAmount(amount)} HBT for ${(duration / DAY).toFixed(2)} days.`;
 
     const gap = width / len;
     const MAX_RELATIVE = rewards[len - 1];
@@ -82,7 +211,7 @@ async function render () {
       ctx.moveTo(0, height);
       let x = 0;
       for (let i = 0; i < len; i++) {
-        const r = rewards[i] / MAX_REWARD;
+        const r = rewards[i] / MAX_REWARD_DAY;
         const y = height - (height * r);
         ctx.lineTo(x, y);
         x += gap;
@@ -94,6 +223,8 @@ async function render () {
   }
 
   wrapListener(input, draw, 'keyup');
+  setInterval(update, 5000);
+  update();
   draw();
 }
 
