11235ca96ba6c3430c70979c6476cfbb0455c9e3
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-06-03 16:28:42 +0000

    scripts - fix order of compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,9 +2,8 @@
 
 set -xe
 
-./scripts/exportAbi.js build/contracts/HabitatV1.json > web/lib/HabitatAbi.js
-./scripts/exportAbi.js build/contracts/ExecutionProxy.json > web/lib/ExecutionProxyAbi.js
-
 for x in src/*/scripts/codegen.js;do $x; done
 
 yarn develatus-apparatus-compile src/*/**/*.sol src/*/**/**/*.sol
+./scripts/exportAbi.js build/contracts/HabitatV1.json > web/lib/HabitatAbi.js
+./scripts/exportAbi.js build/contracts/ExecutionProxy.json > web/lib/ExecutionProxyAbi.js
