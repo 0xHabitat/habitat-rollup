c573ff20e8e0c1e1a388f2740ee005438de44e7f
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-06-03 12:36:09 +0000

    web - use exported abi files

---
+++ web/lib/ExecutionProxyAbi.js
@@ -0,0 +1,7 @@
+export default [
+  'event ProxyCreated(address indexed bridge, address indexed vault, address proxy)',
+  'function createProxy(address bridge, address vault) returns (address addr)',
+  'function execute(bytes32 proposalId, bytes actions)',
+  'function executed(bytes32) view returns (bool)',
+  'function getMetadata() returns (address bridge, address vault)'
+]
