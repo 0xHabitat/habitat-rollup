6550c8aa13fab7f6a7a82eb8a3cfa8195abb1774
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-07-22 07:37:40 +0000

    [skip ci] - optimize images

--- scripts/optimizeImages.sh
+++ scripts/optimizeImages.sh
@@ -8,5 +8,6 @@ if [ $ret -eq 0 ]; then
   apk add pngcrush libjpeg-turbo-utils
 fi
 
-for file in $(find . -name '*.jp*g' | grep -v node_modules/ );do jpegtran -copy all -optimize -progressive -outfile "$file" "$file";done
-for file in $(find . -name '*.png' | grep -v node_modules/ );do pngcrush -reduce -brute -ow "$file";done
+path=${1:-'.'}
+for file in $(find "$path" -name '*.jp*g' | grep -v node_modules/ );do jpegtran -copy all -optimize -progressive -outfile "$file" "$file";done
+for file in $(find "$path" -name '*.png' | grep -v node_modules/ );do pngcrush -reduce -brute -ow "$file";done
