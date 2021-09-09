7414b7c8560a0a73a49501af12c45c00b99ac88e
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-09-09 14:57:07 +0000

    web - cleanup/remove obsolete pages

--- web/lib/HabitatNav.js
+++ web/lib/HabitatNav.js
@@ -30,8 +30,11 @@ const NAV_TEMPLATE =
 <div class='wrapperNav'>
   <div class='flex row center around' style='padding-right:3rem;'>
     <div class='flex col left' style='align-items:flex-start;'>
-      <object type='image/svg+xml' style='height:2rem;' data='/lib/assets/v2-logo-full.svg'></object>
-      <button id='add747' style='margin:.3rem 0;' class='secondary purple smaller noHover'>Add HBT to MetaMask</button>
+      <div class='flex col'>
+        <object type='image/svg+xml' style='height:2rem;' data='/lib/assets/v2-logo-full.svg'></object>
+        <a href='/' style=position:relative;top:-2em;height:2em;width:100%;margin-bottom:-2em;></a>
+      </div>
+      <button id='add747' style='margin:.3rem 0;font-size:.6em;' class='secondary purple noHover'>Add HBT to MetaMask</button>
     </div>
     <div class='flex row evenly'>
       <habitat-color-toggle style='margin:0 1em;'></habitat-color-toggle>
