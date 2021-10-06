71c6b066aa51f4e8377be4573be3eae8216f0725
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-10-06 06:27:38 +0000

    [skip ci] scripts: env FLY_TOKEN, test for empty EXPECTED_CID

--- scripts/publish-web.sh
+++ scripts/publish-web.sh
@@ -12,6 +12,10 @@ fi
 ipfs init
 EXPECTED_CID=$(ipfs --offline add --recursive --hash sha2-256 --cid-version 1 --chunker=size-262144 --only-hash -Q $path | tail -n 1)
 
+if [ -z $EXPECTED_CID ]; then
+  exit 1
+fi
+
 # can't use ipfs because the connection with the ipfs cli to infura doesn't work
 FILES=$(find $path -type f -not -iname ".*")
 args=''
@@ -29,9 +33,9 @@ fi
 
 # update dns
 APEX=`echo ${DOMAIN} | awk -F'.' '{print $(NF-1)"."$(NF)}'`
-/flyctl dns-records export $APEX > .tmp_zone
+FLY_TOKEN=$FLY_TOKEN /flyctl dns-records export $APEX > .tmp_zone
 OLD_CID=`dig +short TXT _dnslink.${DOMAIN} | sed -e 's/dnslink=\/ipfs\///g' -e 's/"//g'`
 cat .tmp_zone
 sed -i'' -e "s/$OLD_CID/$NEW_CID/g" .tmp_zone
 cat .tmp_zone
-/flyctl dns-records import $APEX .tmp_zone
+FLY_TOKEN=$FLY_TOKEN /flyctl dns-records import $APEX .tmp_zone
